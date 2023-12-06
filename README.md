dbab2a9728c2af9252d9c6a606129ea004e2054e
Xiaolin <xiaolin.song@benerail.com>
2023-12-06 13:28:46 +0100

    Run from command line

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ npm install cypress --save-dev
 
 ##### open app
 npx cypress open
+# run from command line
+./node_modules/.bin/cypress run
+./node_modules/.bin/cypress run -s ./cypress/e2e/firsttest.cy.js --headed --browser chrome
+
+
+
---
+++ cypress/e2e/firsttest.cy.js
@@ -0,0 +1,10 @@
+describe('login test scenario', () => {
+    it('Does not so much', () => {
+      expect(true).to.equal(true)
+    })
+})
+describe('Launch Application', function(){
+    it('Launch the test application', () => {
+        cy.visit('https://saucedemo.com')
+    })
+})
